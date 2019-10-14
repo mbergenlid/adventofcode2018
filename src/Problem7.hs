fa4d251d93985e160307b372e525495be0372c9b
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2019-10-14 07:05:56 +0200

    Problem 7, part 2

--- src/Problem7.hs
+++ src/Problem7.hs
@@ -1,14 +1,19 @@
 module Problem7
 (
     Dependencies,
+    Worker(Available),
     empty,
     new,
     add,
     delete,
     firstEmpty,
+    nextStep,
+    tick,
+    allEmpty,
     solveFor,
     input,
-    testInput
+    testInput,
+    solvePart2
 )
 where
 
@@ -16,6 +21,7 @@ import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Set (Set)
 import qualified Data.Set as S
+import Data.Char
 
 type Step = Char
 type Dependency = (Char, Char)
@@ -42,6 +48,12 @@ firstEmpty (Dependencies deps) = _firstEmpty $ M.assocs deps
     where _firstEmpty ((step, s):xs) | S.null s = step
                                      | otherwise = _firstEmpty xs
 
+allEmpty :: Dependencies -> [Step]
+allEmpty (Dependencies deps) = _allEmpty $ M.assocs deps
+    where _allEmpty ((step, s):xs) | S.null s = step : _allEmpty xs
+                                   | otherwise = _allEmpty xs
+          _allEmpty []             = []
+
 delete :: Step -> Dependencies -> Dependencies
 delete s (Dependencies deps) = Dependencies $ M.delete s $ M.map (S.delete s) deps
 
@@ -49,6 +61,42 @@ solveFor :: Dependencies -> [Step]
 solveFor deps
     | Problem7.null deps = []
     | otherwise, let step = firstEmpty deps = step : (solveFor (delete step deps))
+
+
+data Worker = Available | Busy Step Int
+    deriving Show
+
+step :: Worker -> Maybe Step
+step (Busy s _) = Just s
+step _ = Nothing
+
+tick :: Dependencies -> [Worker] -> [Step] -> [Worker] -> (Dependencies, [Worker])
+tick deps ((Busy s 0):ws) steps result    = tick (delete s deps) ws steps (Available:result)
+tick deps ((Busy s c):ws) steps result    = tick deps ws steps ((Busy s (c-1)) : result)
+tick deps (Available:ws) steps result     = tick deps ws steps (Available : result)
+tick deps [] _ result                     = (deps, result)
+
+
+pick :: Dependencies -> [Worker] -> [Step] -> [Worker] -> (Dependencies, [Worker])
+pick deps (Available:ws) (s:steps) result = pick deps ws steps ((Busy s (timeForStep s)) : result)
+pick deps (Available:ws) [] result        = pick deps ws [] (Available : result)
+pick deps (w:ws) steps result             = pick deps ws steps (w:result)
+pick deps [] _ result                     = (deps, result)
+
+timeForStep :: Step -> Int
+timeForStep step = 60 + (ord step) - (ord 'A')
+
+nextStep :: (Dependencies, [Worker]) -> (Dependencies, [Worker])
+nextStep (deps, workers) = pick depsAfterTick workersAfterTick availableSteps []
+    where (depsAfterTick, workersAfterTick) = tick deps workers [] []
+          availableSteps = dropWhile taken $ allEmpty depsAfterTick
+          taken step = elem (Just step) takenSteps
+          takenSteps = map step workersAfterTick
+
+solvePart2 :: Dependencies -> Int -> [(Dependencies, [Worker])]
+solvePart2 deps numWorkers = takeWhile notEmpty $ scanl (\state _ -> nextStep state) (deps, workers) [1..]
+    where notEmpty (dep, _) = not $ Problem7.null dep
+          workers = take numWorkers $ repeat Available
 
 testInput = [
     ('C', 'A'),
